4d04080a9315974d39d1752334598c7bde89799a
CaeldanRodrigues <cmrodrigues@oneshield.com>
2024-02-16 18:55:00 +0530

    added error handling and validation

--- app.py
+++ app.py
@@ -11,9 +11,27 @@ def get_stores():
     return {"stores": list(stores.values())}
 
 
+@app.get('/store/<string:store_id>')
+def get_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort(404, message="store not found")
+
+
 @app.post('/store')
 def create_store():
     store_data = request.get_json()
+    if 'name' not in store_data:
+        abort(
+            400,
+            messge="Bad request. Ensure 'name' is included in the JSON payload"
+        )
+        
+    for store in stores.values():
+        if (store_data['name'] == store['name']):
+            abort(400, message='store already exists')
+
     store_id = uuid.uuid4().hex
     new_store = {**store_data, "id": store_id}
     stores[store_id] = new_store
@@ -26,9 +44,34 @@ def get_all_items():
     return {'items': list(items.values())}
 
 
+@app.get('/item/<string:item_id>')
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="item not found")
+
+
 @app.post('/item')
 def create_item():
     item_data = request.get_json()
+
+    if ('price' not in item_data
+        or 'store_id' not in item_data
+        or 'name' not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id' and 'name' are included in the JSON payload"
+        )
+
+    for item in items.values():
+        if (
+            item_data['name'] == item['name']
+            and item_data['store_id'] == item['store_id'] 
+        ):
+            abort(400, message='item already exists')
+
     if item_data['store_id'] not in stores:
         abort(404, message="store not found")
 
@@ -39,17 +82,25 @@ def create_item():
     return item, 201
 
 
-@app.get('/store/<string:store_id>')
-def get_store(store_id):
+@app.put('/item/<string:item_id>')
+def update_item(item_id):
+    item_data = request.get_json()
+    if 'price' not in item_data or 'name' not in item_data:
+        abort(400, message="Bad request. Ensure 'price'and 'name' are included in the JSON payload")
+
     try:
-        return stores[store_id]
+        item = items[item_id]
+        item |= item_data
+
+        return item
     except KeyError:
-        abort(404, message="store not found")
+        abort(404, message='item not found')
 
 
-@app.get('/item/<string:item_id>')
-def get_item(item_id):
+@app.delete('/item/<string:item_id>')
+def delete_item(item_id):
     try:
-        return items[item_id]
+        del items[item_id]
+        return {'message': 'item deleted'}
     except KeyError:
-        abort(404, message="item not found")
+        abort(404, message='item not found')
