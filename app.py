7cd70bd08138c06cedfea979e421350ad44c0649
CaeldanRodrigues <cmrodrigues@oneshield.com>
2024-02-27 18:22:28 +0530

    added logout functionality

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_jwt_extended import JWTManager
 
 from db import db
 import models
+from blocklist import BLOCKLIST
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
@@ -31,6 +32,19 @@ def create_app(db_url=None):
 
     app.config['JWT_SECRET_KEY'] = "186702302766029508709097997832320513896"
     jwt = JWTManager(app)
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload['jti'] in BLOCKLIST
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
 
     @jwt.expired_token_loader
     def expired_token_callback(jwt_header, jwt_payload):
--- resources/user.py
+++ resources/user.py
@@ -1,11 +1,12 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, get_jwt, jwt_required
 
 from db import db
 from models import UserModel
 from schemas import UserSchema
+from blocklist import BLOCKLIST
 
 blp = Blueprint("Users",  __name__, description='Operations on users')
 
@@ -43,6 +44,14 @@ class UserLogin(MethodView):
         
         abort(401, message='invalid credentials')
 
+
+@blp.route('/logout')
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
 
 @blp.route('/user/<int:user_id>')
 class User(MethodView):
