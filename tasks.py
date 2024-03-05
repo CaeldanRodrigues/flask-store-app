fb47734afd1c901c3202e639022879fb29139dc0
CaeldanRodrigues <caeldanrodrigues@gmail.com>
2024-03-05 19:24:56 +0530

    refactored to send html emails on registering

--- tasks.py
+++ tasks.py
@@ -1,12 +1,18 @@
 import os
 import requests
+import jinja2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 DOMAIN = os.getenv('MAILGUN_DOMAIN')
+template_loader = jinja2.FileSystemLoader('templates')
+template_env = jinja2.Environment(loader=template_loader)
 
-def send_simple_message(to, subject, body):
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+def send_simple_message(to, subject, body, html):
     domain = os.getenv('MAILGUN_DOMAIN')
     return requests.post(
         f"https://api.mailgun.net/v3/{DOMAIN}/messages",
@@ -14,7 +20,8 @@ def send_simple_message(to, subject, body):
         data={"from": f"Caeldan Rodrigues <mailgun@{DOMAIN}>",
             "to": [to],
             "subject": subject,
-            "text": body
+            "text": body,
+            "html": html
         }
     )
 
@@ -23,5 +30,6 @@ def send_user_registration_email(email, username):
     return send_simple_message(
         email,
         "Successfully signed up!",
-        f"Hi {username} ! you have succesfully signed up to the flask rest api store."
+        f"Hi {username} ! you have succesfully signed up to the flask rest api store.",
+        render_template('email/action.html', username=username)
     )
